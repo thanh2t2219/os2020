17c7d17d0452fa47cbd6d717bf4e98b4e669186c
thanh2t2219 <thanh2t2219@yahoo.com>
2020-03-20 21:11:56 +0700

    I dont know if all of my Practical Works are correct, can you check it and tell me where I'm wrong, please. Thank you

--- 08.practical.work.pthread.c
+++ 08.practical.work.pthread.c
@@ -1,3 +1,44 @@
 #include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
 #include<stdlib.h>
 
+void* prime(void * arg) {
+	for(int i=2; i<=1000000; i++) {
+		int count = 0;
+		for(int j=2; j<=i; j++) {
+			if(i%j==0){
+				count++;
+			}
+		}
+		if (count == 1) {
+			sleep(1);
+			printf("Prime number: %d\n", i);
+		}
+	}
+	return NULL;
+}
+
+void fibonacci() {
+	int num = 2, previous = 1;
+	for(int i=0; i<1000000; i++) {
+		sleep(1);
+		printf("%d\n", num);
+		int next = num + previous;
+		previous = num;
+		num = next;
+		
+		if(num >= 1000000){
+			break;
+		}
+	}
+}
+
+int main() {
+	pthread_t newthread;
+
+	pthread_create(&newthread, NULL, prime, NULL);
+	fibonacci();
+
+	pthread_join(newthread, NULL);
+}
